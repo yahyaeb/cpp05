e11142385dc01f1ffbb59fe8dfcb5d90c631c007
yel-bouk <yahya.elboukili1@gmail.com>
2025-08-30 15:50:53 +0200

    completed ex00 cpp05, need to complete 01 - 03

---
+++ Bureaucrat.cpp
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-bouk <yel-bouk@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/30 12:41:51 by yel-bouk          #+#    #+#             */
+/*   Updated: 2025/08/30 15:47:19 by yel-bouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << " Bureaucrat constructor called! " << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name) , _grade(grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if(grade > 150)
+		throw GradeTooLowException();
+	std::cout << "Name and grade constructor called." << std::endl;
+}
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade)
+{}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if (this != &other)
+		_grade = other._grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << " Bureaucrat destructor called! " << std::endl;
+}
+
+void	Bureaucrat::incrementGrade(void)
+{
+	if(_grade == 1)
+		throw GradeTooHighException();
+	else
+		_grade--;
+}
+
+void	Bureaucrat::decrementGrade(void)
+{
+	if(_grade == 150)
+		throw GradeTooLowException();
+	else
+		_grade++;
+}
+
+const std::string& Bureaucrat::getName(void) const
+{
+	return _name;
+}
+int Bureaucrat::getGrade(void) const
+{
+	return _grade;
+}
+
+int main()
+{
+	// try
+	// {
+	// 	Bureaucrat test("Yahya", 148);
+	// 	std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+	// 	std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	// 	test.decrementGrade();
+	// 	std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+	// 	std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	// 	test.decrementGrade();
+	// 	std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+	// 	std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	// 	test.incrementGrade();
+	// 	std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+	// 	std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	// 	int i = 160;
+	// 	while(i > 1 )
+	// 	{
+	// 		test.incrementGrade();
+	// 		if(i == 1)
+	// 		{
+	// 			std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+	// 			std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	// 		}
+	// 		i++;
+	// 	}
+	// 	std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+	// 	std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	// }
+	// catch(const std::exception &e)
+	// {
+	// 	std::cerr << "Exception caught: " << e.what() << std::endl;
+	// }
+	try
+	{
+		Bureaucrat test("Yahya", 1);
+		std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+		std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+		test.incrementGrade();
+		test.incrementGrade();
+		std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+		std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+		test.incrementGrade();
+		std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+		std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+		int i = 160;
+		while(i > 1 )
+		{
+			test.incrementGrade();
+			if(i == 1)
+			{
+				std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+				std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+			}
+			i++;
+		}
+		std::cout << "Current name is => " << test.getName() << "\n" << std::endl;
+		std::cout << "current grade is => " << test.getGrade() << "\n"<< std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
+	
+}
---
+++ Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-bouk <yel-bouk@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/30 12:41:54 by yel-bouk          #+#    #+#             */
+/*   Updated: 2025/08/30 15:39:34 by yel-bouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+
+	public:
+	//constrcutor
+	Bureaucrat();
+	Bureaucrat(const std::string& name, int grade);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
+	
+	const	std::string& getName(void) const;
+	int		getGrade(void) const;
+	void	incrementGrade(void);
+	void	decrementGrade(void);
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw()
+			{
+				return "Grade Too High";
+			}
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw()
+			{
+				return "Grade Too Low";
+			}
+	};
+	friend std::ostream& operator<<(std::ostream& os, const Bureaucrat& x);
+};
